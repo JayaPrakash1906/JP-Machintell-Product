5caff2f52130ce1e895fbedaba3b0723c62fc815
JayaPrakash1906 <jayaprakashtrk8@gmail.com>
2024-04-12 11:54:00 +0530

    .

--- routes/ProductSpecsRoutes/viewproductspecs.js
+++ routes/ProductSpecsRoutes/viewproductspecs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const viewproductspecscontroller = require('../../controller/ProductSpecsController/viewproductspecscontroller');
-router.delete('/:id', viewproductspecscontroller);
+router.get('/:id', viewproductspecscontroller);
 
 module.exports = router;
